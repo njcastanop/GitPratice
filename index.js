bd7aa83074df6a08a99e8ad41be3bbc4466c2fe7
njcastanop <njcastanop@gmail.com>
2022-07-08 19:05:21 -0500

    Initial version GitPractice Umanizales

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+require("dotenv").config()
+const port = process.env.PORT || 5000
+const mongoose = require("mongoose")
+
+app.listen (port, () => {
+    console.log("Puerto Activo:", port);
+})
+
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING)
+.then(() => console.log('Conectado con la base de datos'))
+.catch((err) => console.error(err));
+    
